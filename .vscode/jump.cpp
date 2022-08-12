7357745ccf3824962a36200a5c44b1167ae520e7
jas kohli <jaskaran.1p@gmail.com>
2022-08-12 19:49:57 +0530

    jump

---
+++ .vscode/jump.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int pmoney=3000;
+    for(int date=1;date<=30;date++){
+        if(date%2==0){
+            continue;
+        }
+        if(pmoney==0){
+            break;
+        }
+        cout<<"go out"<<endl;
+        pmoney=pmoney-300;
+    }
+    return 0;
+}
